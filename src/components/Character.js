de795720f451584d1e4f7d8c9a6df0429f3ce37a
Jonathan Herzog <jonathan@Jonathans-MacBook-Pro-3.local>
2022-11-03 19:23:34 -0400

    begin import

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,17 @@
-// Write your Character component here
+import React, { useState } from 'react';
+import App from './App';
+import { setupWorker } from "msw";
+import { handlers } from "./handlers";
+import { setupServer } from "msw/node";
+import { rest } from "msw";
+import './App.css';
+
+const Character = (props) => {
+    const []
+
+return (
+    data.name
+  );
+}
+
+export default Character;
